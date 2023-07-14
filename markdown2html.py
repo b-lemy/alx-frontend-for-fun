1d5da3ad8bba7f8b0d1e4add83de0e5143be5939
b-lemy <lemabrian1234@gmail.com>
2023-07-14 18:24:31 +0300

    correction

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''' Write a script markdown2html.py that takes an argument 2 strings:
+
     First argument is the name of the Markdown file
     Second argument is the output file name
 '''
@@ -94,4 +95,3 @@ if __name__ == '__main__':
                 html.write('</ol>\n')
             if paragraph:
                 html.write('</p>\n')
-    exit (0)
